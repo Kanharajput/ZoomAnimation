62d355e9d35927d46f8adf4bc957137f110bdacd
Kanha Tomar <kanharajput91@gmail.com>
2022-03-15 17:07:06 +0530

    Fling Animations on a view

---
+++ app/src/main/java/com/example/flinganimation/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.flinganimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.dynamicanimation.animation.DynamicAnimation;
+import androidx.dynamicanimation.animation.FlingAnimation;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        image = findViewById(R.id.imageView);
+
+    }
+
+    public void moveThisItem(View view) {
+    FlingAnimation fling = new FlingAnimation(view, DynamicAnimation.SCROLL_Y);
+    fling.setStartVelocity(700f)
+            .setFriction(2.1f)
+            .setMaxValue(600f)         //after this value it will stop the motion lets say at every click it move 100f then after 6 times it will stop
+            .start();
+    }
+
+}
